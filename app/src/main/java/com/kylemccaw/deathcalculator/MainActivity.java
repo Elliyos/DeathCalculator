c4eeef1cd56aebc3d486768de739eec6104359aa
McCaw <kylemccaw15@augustana.edu>
2019-03-05 14:59:08 -0600

    Basic layout with errors

--- app/src/main/java/com/kylemccaw/deathcalculator/MainActivity.java
+++ app/src/main/java/com/kylemccaw/deathcalculator/MainActivity.java
@@ -6,15 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button selectDate;
-    TextView date;
+    Button selectBirthDate, selectDeathDate;
+    TextView birthDate, deathDate, length;
     DatePickerDialog datePickerDialog;
+    NumberPicker years, days;
     int year;
     int month;
     int dayOfMonth;
@@ -25,11 +27,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        selectDate = findViewById(R.id.btnDate);
-        date = findViewById(R.id.date);
+        years.setMinValue(0);
+        years.setMaxValue(125);
+        days.setMinValue(0);
+        days.setMaxValue(365);
+
+        selectBirthDate = findViewById(R.id.btnBirthDate);
+        selectDeathDate = findViewById(R.id.btnDeathDate);
+        birthDate = findViewById(R.id.birthDate);
+        deathDate = findViewById(R.id.deathDate);
+        years = findViewById(R.id.years);
+        days = findViewById(R.id.days);
+        length = findViewById(R.id.length);
 
         // code guidance provided by GitHub user codingdemos
-        selectDate.setOnClickListener(new View.OnClickListener() {
+        selectBirthDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 calendar = Calendar.getInstance();
@@ -40,7 +52,25 @@ public class MainActivity extends AppCompatActivity {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                                date.setText(day + "/" + (month + 1) + "/" + year);
+                                birthDate.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+
+        selectDeathDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                deathDate.setText(day + "/" + (month + 1) + "/" + year);
                             }
                         }, year, month, dayOfMonth);
                 datePickerDialog.show();
